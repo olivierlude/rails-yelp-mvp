55a4167136a59743bf8fa8d1e0e26e513cf11811
olivierlude <olivierlude@wanadoo.fr>
2019-08-14 16:36:20 +0200

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+require 'faker'
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  post = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number:Faker::PhoneNumber.cell_phone_with_country_code ,
+    category: Faker::Restaurant.type,
+    )
+  post.save!
+end
+puts 'Finished!'
+
+
+
+
+
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
